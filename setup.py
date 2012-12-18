5ea68d483313516b7cffb28cf8a9ef37033d89c6
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2012-12-18 16:10:22 +0700

    Add 'setup.py'

---
+++ setup.py
@@ -0,0 +1,17 @@
+# vim: set fileencoding=utf-8 fileformat=unix :
+
+from distutils.core import setup
+
+
+setup(
+    name='autopages',
+    version='0.1',
+    packages=[
+        'autopages',
+    ]
+    install_requires=[
+        'Django',
+    ],
+    description='Application for automatic URL recognition for templates',
+    zip_safe=False,
+)
